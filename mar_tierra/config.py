b5c891b7a790425cbd07d3564a3ea60215817f5a
const_design <manuiglesias55@gmail.com>
2024-10-15 03:00:03 +0000

    Auto-update from VM

--- mar_tierra/config.py
+++ mar_tierra/config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mar_tierra.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/wuarpsdesigns/Mar_y_tierra/mar_tierra/mar_tierra.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
